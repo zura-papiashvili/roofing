21bcb9badc40a3ecb06624c6bc7090ac6b022ebc
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2025-01-26 17:28:40 -0500

    feat: contact send mail

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
-from django.views import View
-from django.views.generic import TemplateView
 from django.shortcuts import render
-
 from .models import Author, Post
+from django.core.mail import send_mail
+from django.conf import settings
+from django.template.loader import render_to_string
+
+from luzysonido.settings import EMAIL_HOST_USER
 
 
 def home(request):
@@ -16,10 +17,39 @@ def about(request):
     return render(request, "about.html", {"authors": authors})
 
 
-def contact(request):
-    return render(request, "contact.html")
+# def contact(request):
+#     return render(request, "contact.html")
 
 
 def posts(request):
     posts = Post.objects.all().order_by("-date")
     return render(request, "blog/posts.html", {"posts": posts})
+
+
+def contact(request):
+    if request.method == "POST":
+        message_name = request.POST["message-name"]
+        message_email = request.POST["message-email"]
+        message = request.POST["message"]
+
+        # Create a nicely formatted HTML message
+        email_message = render_to_string(
+            "email/contact_email.html",  # Create a new template for email content
+            {
+                "message_name": message_name,
+                "message_email": message_email,
+                "message": message,
+            },
+        )
+
+        send_mail(
+            f"Message from {message_name}",  # Subject
+            "",  # No plain text message
+            message_email,  # From email
+            [settings.EMAIL_HOST_USER],  # To email
+            html_message=email_message,  # HTML message content
+        )
+
+        return render(request, "contact.html", {"message_name": message_name})
+
+    return render(request, "contact.html", {})
--- luzysonido/settings.py
+++ luzysonido/settings.py
@@ -197,3 +197,14 @@ DATABASES = {
         "PORT": env("DB_PORT"),
     }
 }
+
+
+# Email configuration
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
